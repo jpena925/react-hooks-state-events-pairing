d98f88ea50ab55768df31bb50b91d857985aff14
Jack Pena <penajackc@gmail.com>
2022-02-03 09:33:33 -0700

    added components and finished video.js

---
+++ src/components/MainContent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MainContent() {
+  return <div></div>;
+}
+
+export default MainContent;
---
+++ src/components/Video.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Video() {
+  return (
+    <div>
+        <iframe
+        width="919"
+        height="525"
+        src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+        frameBorder="0"
+        allowFullScreen
+        title="Thinking in React"
+        />
+    </div>);
+}
+
+export default Video;
